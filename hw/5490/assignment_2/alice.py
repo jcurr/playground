2b5b0579b6dd95a5d579e8cc3b5cac5ac7d2b1a1
Jonathan Curry <jonathancurry@Jonathans-MacBook.local>
2015-11-05 22:28:45 -0700

    i think it works, need comments, fake cipher choice

--- hw/5490/assignment_2/alice.py
+++ hw/5490/assignment_2/alice.py
@@ -2,11 +2,13 @@ import socket
 from Crypto.Random import random
 from Crypto import Random
 from Crypto.Cipher import PKCS1_v1_5
-from Crypto.Cipher import DES3
+from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA
 import pickle
 import sys
 from common_util import * 
+import subprocess
+import OpenSSL
 
 global msg_1
 global msg_2
@@ -19,10 +21,13 @@ global bob_cert
 global cipher
 global master_secret
 global bob_public_cipher
+global my_enc_key
+global my_auth_key
+global iv
 
 no_errors = True
+my_public_key = RSA.importKey(open('public_alice.pem').read())
 
-my_public_key = extract_public_key('alice_cert.pem')
 my_private_key = RSA.importKey(open('private_alice.pem').read())
 my_private_cipher = PKCS1_v1_5.new(my_private_key)
 my_public_cipher = PKCS1_v1_5.new(my_public_key)
@@ -31,8 +36,9 @@ hash_string = "CLIENT"
 
 #produce random strings of bits for a nonce
 
-nonce_a = random.getrandbits(64)
-print("NA = " + str(nonce_a))
+nonce_a = Random.get_random_bytes(32)
+nonce_a_int = int(nonce_a.encode('hex'), 16)
+print("Na = " + str(nonce_a_int))
 
 #set up socket
 port = int(sys.argv[1])
@@ -46,9 +52,8 @@ while True:
 	if comm_state == 1:
 		req = "I want to talk to you"
 		cipher_options = "ciphers i support"
-		my_cert_string = open("alice_cert.pem").read()
+		my_cert_string = open("a_cert.pem").read()
 
-		#nonce_a = bob_public_cipher.encrypt(nonce_a)
 		msg_1 = pickle.dumps([req, cipher_options, my_cert_string])
 
 		s.send(msg_1)
@@ -63,21 +68,32 @@ while True:
 		bob_cert = open('cert_from_bob.pem', 'w')
 		bob_cert.write(bobs_cert_string)
 		bob_cert.close()
-		bob_public_key = extract_public_key("cert_from_bob.pem")
+		
+		bob_public_key = subprocess.check_output(["openssl","req","-in","cert_from_bob.pem","-noout","-pubkey"])
+		bob_public_key = RSA.importKey(bob_public_key)
 		bob_public_cipher = PKCS1_v1_5.new(bob_public_key)
+
+		bob_csr = OpenSSL.crypto.load_certificate_request(OpenSSL.crypto.FILETYPE_PEM, bobs_cert_string)
+		
+		result ="Success - Bob's cert was verified"
+		if not bob_csr.verify(bob_csr.get_pubkey()):
+			result = "Error - Bob's cert not verifed ******"
+			no_errors = False
+
 		chosen_cipher = msg_arr[1]
 
 		nonce_b_cipher_text = msg_arr[2]
 		sentinel = Random.new().read(15+len(nonce_b_cipher_text))
 		nonce_b = my_private_cipher.decrypt(nonce_b_cipher_text, sentinel)
 
-		print_state_data(comm_state,"recv:\tmy certificate\n\tchosen cipher\n\tKa+{" + str(nonce_b) + "}")
+		nonce_b_int = int(nonce_b.encode('hex'), 16)
+		print_state_data(comm_state,"recv:\tmy certificate\n\tchosen cipher\n\tKa+{" + str(nonce_b) + "}" + "\n" + result)
 		comm_state += 1
 
 	if comm_state == 3:
 		
-		a = nonce_a
-		b = long(nonce_b)
+		a = nonce_a_int
+		b = nonce_b_int
 		master_secret = a ^ b
 		
 		secret = "Master Secret created = " + str(master_secret)
@@ -108,12 +124,33 @@ while True:
 			result = 'Error - Hashes did not match *******************'
 			no_errors = False
 		
+		keys = generate_keys(master_secret, hash_string)
+		my_enc_key = keys[0]
+		my_dec_key = keys[1]
+		my_auth_key = keys[2]
+		my_verify_key = keys[3]
+		iv = keys[4]
+	
+		print(my_enc_key + " " +  my_dec_key + " " + my_auth_key + " " + my_verify_key)
+			
 		print_state_data(comm_state, "recv: digest = " + digest + "\n" + result)
 		comm_state += 1
 
 	# data phase
 	if comm_state == 5:
-				
+		
+		file_to_send = open("original_file.txt").read()
+		to_digest = file_to_send
+		digest = HMAC.new(my_auth_key, to_digest, SHA.new())
+		digest = digest.hexdigest()
+		
+		session_cipher = AES.new(my_enc_key, AES.MODE_CBC, iv)	
+		plain_text = file_to_send + digest
+		plain_text = add_padding(plain_text)
+		cipher_text = session_cipher.encrypt(plain_text)
+		
+		header = b'cMmll'	
+		s.send(header + cipher_text)				
 		comm_state += 1
 		break
 s.close()
--- hw/5490/assignment_2/bob.py
+++ hw/5490/assignment_2/bob.py
@@ -3,11 +3,13 @@ from Crypto import Random
 from Crypto.Random import random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
-from Crypto.Cipher import DES3
+from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA
 import pickle
 import sys
 from common_util import * 
+import OpenSSL
+import subprocess
 
 global msg_1
 global msg_2
@@ -20,19 +22,22 @@ global alice_cert
 global cipher
 global master_secret
 global alice_public_cipher
+global my_dec_key
+global my_verify_key
+global iv
 
 no_errors = True
 
-my_public_key = extract_public_key('bob_cert.pem')
+my_public_key = RSA.importKey(open('public_bob.pem').read())
 my_private_key = RSA.importKey(open('private_bob.pem').read())
 my_private_cipher = PKCS1_v1_5.new(my_private_key)
 my_public_cipher = PKCS1_v1_5.new(my_public_key)
 
 hash_string = "SERVER"
 
-#produce random strings of bits for a nonce
-nonce_b = random.getrandbits(64)
-print("NA = " + str(nonce_b))
+nonce_b = Random.get_random_bytes(32)
+nonce_b_int = int(nonce_b.encode('hex'), 16)
+print("Nb = " + str(nonce_b_int))
 
 #set up socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,7 +75,17 @@ while True:
 		alice_cert = open('cert_from_alice.pem', 'w')
 		alice_cert.write(alices_cert_string)
 		alice_cert.close()	
-		alice_public_key = extract_public_key("cert_from_alice.pem")
+	
+		alice_public_key = subprocess.check_output(["openssl","req","-in","cert_from_alice.pem","-noout","-pubkey"])
+		alice_public_key = RSA.importKey(alice_public_key)
+
+		alice_csr = OpenSSL.crypto.load_certificate_request(OpenSSL.crypto.FILETYPE_PEM, alices_cert_string)
+		
+		result ="Success - Alice's cert was verified"
+		if not alice_csr.verify(alice_csr.get_pubkey()):
+			result = "Error - Alice's cert not verifed ******"
+			no_errors = False
+		
 		alice_public_cipher = PKCS1_v1_5.new(alice_public_key)
 		
 		print_state_data(comm_state, "recv: [I wan't to talk to you, [Alice's ciphers], certificate_alice]")
@@ -79,14 +94,14 @@ while True:
 
 	if comm_state == 2:
 		#Send Bob's certificate to alice, Bob's selected cipher and Bob's nonce
-		my_cert = open("bob_cert.pem").read()
+		my_cert = open("b_cert.pem").read()
 		
 		encrypted_nonce = alice_public_cipher.encrypt(str(nonce_b))
 		
 		msg_2 = pickle.dumps([my_cert, "chosen_cipher", encrypted_nonce])
 		
 		conn.send(msg_2)
-		print_state_data(comm_state,"send:\tmy certificicate\n\tchosen cipher\n\tKa+{" + str(nonce_b) + "}")
+		print_state_data(comm_state,"send:\tmy certificicate\n\tchosen cipher\n\tKa+{" + str(nonce_b) + "}" + result)
 		comm_state += 1
 		continue
 
@@ -99,8 +114,9 @@ while True:
 		nonce_a = my_private_cipher.decrypt(nonce_a_cipher_text, sentinel)
 		
 		
-		a = long(nonce_a)
-		b = nonce_b
+		nonce_a_int = int(nonce_a.encode('hex'), 16)
+		a = nonce_a_int
+		b = nonce_b_int
 		master_secret = a ^ b
 
 		secret = "Master Secret created = " + str(master_secret)
@@ -128,16 +144,52 @@ while True:
 		
 		msg_4 = pickle.dumps([digest])
 		conn.send(msg_4)
+
+		keys = generate_keys(master_secret, hash_string)
+		my_enc_key = keys[0]
+		my_dec_key = keys[1]
+		my_auth_key = keys[2]
+		my_verify_key = keys[3]
+		iv = keys[4]
+
+		print(my_enc_key + " " +  my_dec_key + " " + my_auth_key + " " + my_verify_key)
+
 		print_state_data(comm_state, "send: digest = " + digest)
 		comm_state += 1
 		continue
 
 	# data phase
 	if comm_state == 5:
+		incoming = open("incoming_transfer.txt", "w")
+		msg = conn.recv(packet_size)
+		header = msg[:5]
+		msg = msg[5:]	
+		while msg:
+			incoming.write(msg)
+			msg = conn.recv(packet_size)
+		incoming.close()
+
+		cipher_text = open("incoming_transfer.txt").read()
+
+		session_cipher = AES.new(my_dec_key, AES.MODE_CBC, iv)	
+		plain_text = session_cipher.decrypt(cipher_text)
+		plain_text = remove_padding(plain_text)
+	
+		plain_file = open("incoming_transfer.txt","w")
+		plain_file.write(plain_text[:-40])
+		plain_file.close()		
+		to_digest = plain_text[:-40]
+		digest = HMAC.new(bytes(my_verify_key), to_digest, SHA.new() )
+		digest = digest.hexdigest()
 		
+		alice_digest = plain_text[-40:]
+		result = 'Success - Keyed Hashes matched'
+		if alice_digest != digest:
+			result = 'Error - Hashes did not match *******************'
+			no_errors = False
+		
+		print_state_data(comm_state, "recv: file\n" + result)
 		break
-	#	comm_state += 1
-#		continue
 s.close()
 if no_errors:
 	print '\n\nNo Errors Detected, Have a Nice Day :)\n'
